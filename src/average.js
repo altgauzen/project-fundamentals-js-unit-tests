638069384ef024c905ddf03db7325f3dd26b9193
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-22 19:23:49 +0100

    Requisito 1 - testando

--- src/average.js
+++ src/average.js
@@ -12,8 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let sum = 0;
+  for(let index = 0; index < array.length; index +=1) {
+    if (typeof(array[index]) !== 'number') {
+      return undefined;
+    } else {
+      sum += array[index];
+    }
+  }
+  let averageArray = sum / array.length;
+  return averageArray
 };
 
 module.exports = average;
