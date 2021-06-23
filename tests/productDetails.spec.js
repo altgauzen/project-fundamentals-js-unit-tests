be412c3be062cb6fc467365a39fd3b6d0d277ffe
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-23 21:05:38 +0100

    Requisito 6 finalizado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    // Encontrei essa sacada do Array.isarray numa tread do slack de Lucas Allan - Turma 5. Gratidao.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetails('Alcool gel', 'Máscara')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(Object.values(productDetails('Alcool gel')), Object.values(productDetails('Máscara')));
     // Teste que os dois productIds terminam com 123.
+    //assert.strictEqual(Object.values(productDetails('Alcool gel').productId.substring(productId.length -3)), Object.values(productDetails('Máscara').productId.substring(productId.length -3)));
+    //assert.strictEqual(Object.values(productDetails('Alcool gel', 'Máscara')[0].details.productId.substring(productId.length -3)), Object.values(productDetails('Alcool gel', 'Máscara')[1].details.productId.substring(productId.length -3)));
+    //assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.substring(productId.length -3), productDetails('Alcool gel', 'Máscara')[1].details.productId.substring(productId.length -3));
+    assert.ok(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'));
+    assert.ok(productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123'));
   });
 });
